e352757a77d9075bd56ad31392d91965298c288c
Naresh Petkar <95120287+naresh-ai@users.noreply.github.com>
2024-12-19 14:09:11 +0530

    Update Test.java

--- src/main/java/org/example/test/Test.java
+++ src/main/java/org/example/test/Test.java
@@ -3,5 +3,8 @@ package org.example.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello test");
+
+        System.out.println("hello naresh");
+        System.out.println("hello sameer");
     }
 }
